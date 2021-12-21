71d343a64c3715f7c812791e3d5de797861274c6
Miguel Salinas Gancedo <masalinas.gancedo@gmail.com>
2021-12-21 20:30:38 +0100

    last commit

--- src/main/java/io/oferto/business/config/KeycloakSecurityConfig.java
+++ src/main/java/io/oferto/business/config/KeycloakSecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
-
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
@@ -25,10 +24,11 @@ public class KeycloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter
         super.configure(http);
         
         http.csrf().disable()
-        	.cors().disable()
-	            .authorizeRequests()
-	            .anyRequest()
-	            .authenticated();              
+        	.cors()
+        	.and()
+            .authorizeRequests()
+            .anyRequest()
+            .authenticated();              
     }
 
     // Disable default role prefix ROLE_
